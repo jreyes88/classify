4f26ca92daa130c25e6ea76d197814ff5ca17f97
Chance Eakin <chance.eakin@gmail.com>
2016-08-03 10:40:53 -0500

    merge conflicts

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 // express package
 var express = require('express');
 var routes = require('./controllers/classify-controller.js');
+
 // set up express app
 var app = express();
 app.use('/', routes);
+
+// require and use express-fileupload
+// -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
+// not working
+// -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
+// var fileUpload = require('express-fileupload');
+// app.use(fileUpload());
+
+var multipart = require('connect-multiparty');
+
+app.use('/home/teacher/fileUpload', multipart());
 
 // dependencies for post requests
 var bodyParser = require('body-parser');
